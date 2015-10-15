99af075fdafc2f87047dcaf21612698142273beb
Evelyn Gustavsson <Evy@Evelyns-MacBook-Air.local>
2015-10-15 14:53:07 +0200

    no message

---
+++ src/main/java/com/moviestar/entity/Envelope.java
@@ -0,0 +1,17 @@
+package com.moviestar.entity;
+
+public class Envelope {
+	private Trailer trailer;
+
+	public Envelope() {
+		
+	}
+	
+	public Trailer getTrailer() {
+		return trailer;
+	}
+
+	public void setPlaylist(Trailer trailer) {
+		this.trailer = trailer;
+	}
+}
--- src/main/java/com/moviestar/service/TrailerService.java
+++ src/main/java/com/moviestar/service/TrailerService.java
@@ -11,20 +11,21 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClients;
 import com.google.gson.Gson;
+import com.moviestar.entity.Envelope;
 import com.moviestar.entity.Movie;
 import com.moviestar.entity.Trailer;
 
 public class TrailerService {
 	private static Trailer trailer;
-	private static Movie movie;
+//	private static Movie movie;
+	private static Envelope envelope;
 	
 	public static void main(String[] args) {
 		
 //		String url = "http://api.sr.se/api/v2/playlists/rightnow?channelid=2576&format=json";
-
 //		String url = "http://api.traileraddict.com/";
-		String url = "http://www.traileraddict.com/trailerapi";
-		// Saknas förmodligen något i länken.
+//		String url = "http://www.traileraddict.com/trailerapi";
+		String url = "http://api.traileraddict.com/";
 
 		HttpClient httpClient = null;
 		HttpGet httpGet = null;
@@ -36,39 +37,34 @@ public class TrailerService {
 		Gson json = new Gson();
 
 		try {
-			// Create the client that will call the API
 			httpClient = HttpClients.createDefault();
 			httpGet = new HttpGet(url);
 
-			// Call the API and verify that all went well
 			response = httpClient.execute(httpGet);
 			status = response.getStatusLine();
 			if (status.getStatusCode() == 200) {
-				// All went well. Let's fetch the data
+
 				entity = response.getEntity();
 				data = entity.getContent();
 
 				try {
-					// Attempt to parse the data as JSON
+					// Omvandlar datan till Json.
 					reader = new InputStreamReader(data);
 					
 					trailer = json.fromJson(reader, Trailer.class);
 					
-//					envelope = json.fromJson(reader, Envelope.class);
-//					trailer = envelope.getPlaylist();
+					envelope = json.fromJson(reader, Envelope.class);
+					trailer = envelope.getTrailer();
 
-					// Print the info
 					printTrailer(trailer.getLink());
 					if (trailer.getLink() != null) {
 						printTrailer(trailer.getLink());
 					}
 				} catch (Exception e) {
-					// Something didn't went well. No calls for us.
 					e.printStackTrace();
 					System.out.println("Something went wrong!");
 				}
 			} else {
-				// Something didn't went well. No calls for us.
 				System.out.println("Something went wrong!");
 			}
 		} catch (IOException e) {
