ede426560e95c4555ae18328478d7bab2fe2acef
Evelyn Gustavsson <Evy@Evelyns-MacBook-Air.local>
2015-10-21 15:07:55 +0200

    no message

--- src/main/java/com/moviestar/entity/Trailer.java
+++ src/main/java/com/moviestar/entity/Trailer.java
@@ -1,43 +1,38 @@
 package com.moviestar.entity;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
 /**
  * Klass som avänds för att hämta en inre tag till trailern.
+ * 
  * @author Evy
  *
  */
-//@XmlRootElement(name="trailer")	//HÄNVISAR TILL TAG<trailer>
+@XmlRootElement(name = "trailer") // Hänvisar till taggen "trailer"
 public class Trailer {
 	private String movieTitle;
 	private String trailerID;
-//	private String link; // Ta eventuellt bort.
-//	
-//	public Trailer(String link) {
-//		this.link = link;
-//	}
-//	
-//	public String getLink() {
-//		return link;
-//	}
-	
-//	@XmlElement
+
+	@XmlElement
 	public void setMovieTitle(String movieTitle) {
 		this.movieTitle = movieTitle;
 	}
-	
-//	@XmlElement
+
+	@XmlElement
 	public void setTrailerID(String trailerID) {
 		this.trailerID = trailerID;
 	}
-	
+
 	public String getMovieTitle() {
 		return this.movieTitle;
 	}
-	
+
 	public String getTrailerID() {
 		return this.trailerID;
 	}
-	
+
 	public String toString() {
-		return "Title: " + movieTitle + "TrailerID: " + trailerID;
+		return "Title: " + movieTitle + "\nTrailerID: " + trailerID;
 	}
 }
