853e73b63ebd2d68395e000e44c1a3c007230435
Emma Shakespeare <emma@wikblad.com>
2015-10-29 13:51:46 +0100

    rensat upp movieService

--- src/main/java/com/moviestar/service/MovieService.java
+++ src/main/java/com/moviestar/service/MovieService.java
@@ -1,7 +1,4 @@
 package com.moviestar.service;
-/**
- * Fick något att funka iaf  nudå!
- */
 import java.io.IOException;
 import java.util.Map;
 
@@ -27,103 +24,22 @@ import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 
 /**
- * 
+ * Klassen hämtar information från api omdb.com
  * @author emmashakespeare, evelyn, juicy
  *
  */
 @Path("/collection")
 public class MovieService {
 
-//	 @Path("/bestmovie")
-//	 @GET
-//	 @Produces(MediaType.APPLICATION_JSON)
-//	 public Response getMovie(){
-//	 Response rs = null;
-//	
-//	
-//	 Movie movieObject = new Movie();
-//	 JSONArray ja = new JSONArray();
-//	 JSONObject jo = new JSONObject();
-//	 ToJSON converter = new ToJSON();
-//	 Map<String, Object> map;
-//	
-//	 //Skapa ett objekt
-//	 movieObject.setActor("Kiera Knightley");
-//	 movieObject.setTitle("Pirates of the carribean");
-//	 movieObject.setDirector("Who knows");
-//	 movieObject.setPlot("Johnny Depp takes on the world again");
-//	 map = movieObject.getMap();
-//	
-//	 jo = converter.toJSONArray(map);
-//	 ja.put(jo);
-//	
-//	 movieObject.setActor("Billy Boo");
-//	 movieObject.setTitle("And on he take sit again");
-//	 movieObject.setDirector("Willy Weiner");
-//	 movieObject.setPlot("Three en are stuck in the dessert....");
-//	 map = movieObject.getMap();
-//	
-//	 jo = converter.toJSONArray(map);
-//	 ja.put(jo);
-//	
-//	 rs = Response.ok(ja.toString()).build();
-//	
-//	 return rs;
-//	 }
-
-	@Path("/trailer/{movieTitle}")
-	@GET
-	@Produces(MediaType.APPLICATION_XML)
-	public String getTrailer(@PathParam("movieTitle") String movieTitle){
-		String title = "";
-		HttpResponse<JsonNode> response;
-		
-		try{
-			
-			
-			response = Unirest.get("http://api.traileraddict.com")
-					.queryString("format", "xml")
-					.queryString("actor", movieTitle).asJson();
-			
-			JsonNode json = response.getBody();
-			JSONObject envelope = json.getObject();
-			
-			System.out.println(response.getBody());
-			title = envelope.getString("title");
-
-			
-		} catch (UnirestException e) {
-			e.printStackTrace();
-		}
-		return title;
-
-	}
-	
-	//testar att komenterar.//Jody
-	
-	
 	@Path("/{movietitle}")
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public String getMovie(@PathParam("movietitle") String movieTitle) { // JSONArray movies = new JSONArray();
-		String plot= "";
-		String title= "";
-		String director= "";
-		String year= "";
+	public String getMovie(@PathParam("movietitle") String movieTitle) { 
 		String result = "";
-
 		HttpResponse<JsonNode> response;
-		// HttpResponse<JsonNode> response2;
 
 		try {
-			// response2 =
-			// Unirest.get("http://api.traileraddict.com/trailerapi").queryString("format",
-			// "json")
-			// .queryString("film", "Interstellar").asJson();
-			// System.out.println(response2.getBody());
-			// JsonNode json2 = response2.getBody();
-			// JSONObject envelope2 = json2.getObject();
-			// System.out.println("Länk: " + envelope2.getString("link"));
+			
 
 			response = Unirest.get("http://www.omdbapi.com")
 					.queryString("format", "json")
@@ -133,32 +49,15 @@ public class MovieService {
 			System.out.println(response.getBody());
 			JsonNode json = response.getBody();
 			JSONObject envelope = json.getObject();
-			// System.out.println("Titel: " + envelope.getString("Title"));
-			title = envelope.getString("Title");
-			year = envelope.getString("Year");
-			director = envelope.getString("Director");
-			plot = envelope.getString("Plot");
-		//	result = "Title: " + title + "\nYear: " + year + "\nDirector: " + "\nPlot: " + plot;
 			
 			result = envelope.toString();
 
-			// Unirest.shutdown();
+			 Unirest.shutdown();
 		} catch (UnirestException e) {
 			e.printStackTrace();
-			// } catch (IOException e) {
-			// e.printStackTrace();
-			// }
-
-			// HttpResponse<JsonNode> response2;
-
-			// response2 =
-			// Unirest.get("http://api.traileraddict.com/trailerapi").queryString("format",
-			// "json")
-			// .queryString("film", "Interstellar").asJson();
-			// System.out.println(response2.getBody());
-			// JsonNode json2 = response2.getBody();
-			// JSONObject envelope2 = json2.getObject();
-			// System.out.println("Länk: " + envelope2.getString("link"));
+			
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 		return result;
 		
